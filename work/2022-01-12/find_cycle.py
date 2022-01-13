b409eb919e9d2b4a648df0fba91a8ce3c7053f4b
Michael Benedict <thebenedict@gmail.com>
2022-01-12 19:23:13 -0800

    bipartite graphs, dfs cycle detection

---
+++ work/2022-01-12/find_cycle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+'''
+Determine if a simple, undirected graph has a cycle
+'''
+
+def main():
+    graphs = [
+        ([[1,4,5],[0,2],[1,3],[2,4],[0,3],[0]], True),
+        ([[1,4,5],[0,2,3],[1],[1],[0],[0]], False)
+    ]
+
+    for g, result in graphs:
+        out = has_cycle(g)
+        print(g, out)
+        assert out == result
+
+def has_cycle(g):
+    discovered = set()
+    parent = [-1] * len(g)
+    return dfs(g, 0, parent, discovered)
+
+def dfs(g: list, node: int, parent: list, discovered: set) -> bool:
+    discovered.add(node)
+    for neighbor in g[node]:
+        if neighbor in discovered:
+            if parent[node] != neighbor:
+                return True
+        else:
+            parent[neighbor] = node
+            if dfs(g, neighbor, parent, discovered):
+                return True
+    return False
+    
+if __name__ == "__main__":
+    main()
+
